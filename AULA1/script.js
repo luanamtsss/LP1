7f1992682d171a70cacb6d13d3f45fef1f00b206
gholyra <isa.bellekong2101@gmail.com>
2024-09-04 12:58:03 -0300

    html e js

---
+++ AULA1/script.js
@@ -0,0 +1,27 @@
+// math.around arredondamento comum
+console.log(math.round(numero))
+ 
+//math.ceil arredonda sempre pra cima
+console.log(math.ceil(numero))
+ 
+//math.floor arredonda sempre pra baixo
+console.log(math.floor(numero))
+ 
+//math.pow gera potenciaçao
+console.log(math.pow(5,2))
+ 
+//math.sqrt gera raiz quadrada
+console.log(math.sqrt(35))
+ 
+//math.abs torna qualque numero positivo
+console.log(math.abs(-5))
+ 
+//math.min encontra o menor numero 
+console.log(math.min(7,45,89,2,125))
+ 
+//math.max encontra o maior numero 
+console.log(math.max(7,45,89,2,125))
+ 
+let numeroAleatorio = Math.random()
+console.log(numeroAleatorio)
+ 
